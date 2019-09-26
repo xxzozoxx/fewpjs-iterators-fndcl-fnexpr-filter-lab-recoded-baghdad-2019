e62d1c026dbfcade50adca96c15e59057368e921
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-26 19:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (arr,s){
+  arr= arr.filter(function(v){
+    return v.toLowerCase() === s.toLowerCase();
+  })
+  return arr;
+}
+function fuzzyMatch(arr,s){
+   arr= arr.filter(function(v){
+    return v.toLowerCase().indexOf(s.toLowerCase()) !== -1;
+  })
+  return arr;
+}
+function matchName(arr,s) {
+  arr = arr.filter(function(v){
+    return v['name'] === s });
+    return arr;
+}
